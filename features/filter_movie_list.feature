1a354aba4ea3bd7447db880ec73d19f19c7d6a68
Michael Armida <me@marmida.org>
2012-03-20 12:39:33 -0700

    filter_movie_list.feature should pass, but is failing on my machine - maybe it works for the autograder; the problem is in the session remembering for queries with no ratings checkboxes selected

--- features/filter_movie_list.feature
+++ features/filter_movie_list.feature
@@ -23,13 +23,25 @@ Background: movies have been added to database
   
 Scenario: restrict to movies with 'PG' or 'R' ratings
   # enter step(s) to check the 'PG' and 'R' checkboxes
+  When I check the following ratings: PG, R
   # enter step(s) to uncheck all other checkboxes
+  And I uncheck the following ratings: G, PG-13, NC-17
   # enter step to "submit" the search form on the homepage
+  And I press "Refresh"
   # enter step(s) to ensure that PG and R movies are visible
+  Then I should be on the RottenPotatoes home page
   # enter step(s) to ensure that other movies are not visible
+  And I should not see "The Help"
+  And I should not see "Aladdin"
 
 Scenario: no ratings selected
-  # see assignment
+  When I uncheck the following ratings: G, PG-13, NC-17, PG, R
+  And I press "Refresh"
+  Then I should be on the RottenPotatoes home page
+  And I should see all of the movies
 
 Scenario: all ratings selected
-  # see assignment
+  When I check the following ratings: G, PG-13, NC-17, PG, R
+  And I press "Refresh"
+  Then I should be on the RottenPotatoes home page
+  And I should see all of the movies
--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,8 +4,12 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.new({
+      :title => movie['title'],
+      :rating => movie['rating'],
+      :release_date => movie['release_date'],
+    }).save
   end
-  assert false, "Unimplmemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,7 +18,7 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.content  is the entire content of the page as a string.
-  assert false, "Unimplmemented"
+  assert Regexp.new('%s.*%s' % [t1, t2]).match page.content
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -25,4 +29,15 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+
+  rating_list.scan(/[\w-]+/).each do |rating|
+    step "I %scheck \"ratings[%s]\"" % [uncheck, rating]
+  end
+end
+
+
+# "everything" step
+Then /I should see all of the movies/ do 
+  # there's one extra TR for the header row
+  assert_equal Movie.count + 1, page.all('#movies tr').length
 end
